146756c7f254653fc564ee16661bb63c73a3fe00
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-30 23:04:23 +0500

    chapter-2-completed

--- chapter2/script.js
+++ chapter2/script.js
@@ -1,1 +1,40 @@
 console.log('This is chapter2');
+
+// ** answer 1:
+var userName;
+
+// ** answer 2:
+var myName = "sharjeel ahmed";
+// ** answer 3:
+
+
+let message = "hello world";
+// alert(message);
+
+// ** answer 4:
+var studentName = "sharjeel";
+var studentAge = 21;
+var studentCourse = "web and mobile app development";
+
+
+// alert(studentName);
+// alert(studentAge + "years olds");
+// alert(studentCourse);
+// ** answer 5:
+
+// alert('PIZZA\nPIZZ\nPIZ\nPI\nP');
+// ** answer 6:
+
+var email = "sharjeelalibari3@gmail.com";
+// alert("my email address is " + email);
+// ** answer 7:
+
+var book = "a smart way to learn javascript";
+// alert("i am to trying to learn from the book" + book);
+// ** answer 8:
+document.write("yeah i can write html content thorught javascirpt");
+// ** answer 9:
+
+var icon = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(icon);
