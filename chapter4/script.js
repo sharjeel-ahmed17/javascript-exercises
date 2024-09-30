deab8a51b8d1917449548adc960941cd17148b03
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-30 23:30:22 +0500

    chapter-4-completed

--- chapter4/script.js
+++ chapter4/script.js
@@ -1,1 +1,25 @@
 console.log('This is chapter4');
+
+// ?  answer 1:
+let x, y, z;
+
+// ?  answer 2:
+
+// ! legel
+// let user;
+// let userName ;
+// let $userEmail;
+// let _userPassword;
+// let user_information_details;
+
+// ! illegal
+// let @emeail;
+// let user-name;
+// let user name;
+// let 1stName;
+// let var;
+
+
+// ?  answer 3:
+
+// place in html file 
