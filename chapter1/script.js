7b865591d7236266953e9906de004644afbcddde
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-30 22:51:44 +0500

    chapter-1-completed

--- chapter1/script.js
+++ chapter1/script.js
@@ -1,9 +1,15 @@
 console.log('This is chapter1');
 
 // ? answer 1 :
-alert()
+// alert("welcome to my website");
 // ? answer 2 :
+// alert('error please enter a valid password');
+
 // ? answer 3 :
+// alert('welcome to js land\nhappy coding')
 // ? answer 4 :
+// alert('welcome to js land ');
+// alert('happy coding\nprevant this page for creating additional dialogue');
 // ? answer 5 :
+alert('hello... ican run js thought my web browser console ');
 // ? answer 6 : 
