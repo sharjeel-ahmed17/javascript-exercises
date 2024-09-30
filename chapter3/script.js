766c06e18dd713a1389b3ea2b1485cdcf07a7a86
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-30 23:20:02 +0500

    chapter-3-completed

--- chapter3/script.js
+++ chapter3/script.js
@@ -1,1 +1,19 @@
 console.log('This is chapter3');
+
+// ? answer 1:
+let age = 21;
+// alert("i am " + age + "years old");
+// ? answer 2:
+// later
+
+// ? answer 3:
+
+let birthYear = 2003;
+// document.write('my birth year is '  lare variable is number');
+// ? answer 4: 
+
+let visitorName = "sharjeel ahmed";
+let productTitle = "T-shirt";
+let quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on xyz Clothings store");
